9b2b0c5837aeee085c71115266d41a211aab2fec
kumarnutan204 <kumarnutan204@gmail.com>
2024-02-07 11:52:44 +0530

    [Nutan] Added:solved adding key-value to dictionary and pushed

---
+++ addKeys.py
@@ -0,0 +1,8 @@
+my_dict= {'a':10,'b':20,'age':26, 'wage':5000}
+key_val=input("Enter the key of the new item")
+valForKey=input("Enter the value for the new key")
+
+my_dict[key_val]=valForKey
+
+print("The updated dictionary after adding new key value is: ")
+print(my_dict)
